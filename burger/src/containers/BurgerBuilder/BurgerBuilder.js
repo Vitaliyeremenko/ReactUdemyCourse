fa1807b89a7f27ec1fe1cc4f115118a98363439a
Виталий <eremenkoV@i.ua>
2018-07-17 22:53:46 +0300

    Update burger builder

---
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
+
+class BurgerBuilder extends Component{
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        },
+        totalPrice : 4
+    };
+
+    addIngredientHandler = (type) =>  {
+        const oldCount = this.state.ingredients[type];
+        const updatedCounted = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCounted;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        });
+    };
+
+    removeIngredientHandler = (type) => {
+
+    };
+
+    render(){
+
+        return (
+            <Aux>
+                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                />
+            </Aux>
+        );
+
+    }
+
+}
+
+export default BurgerBuilder;
